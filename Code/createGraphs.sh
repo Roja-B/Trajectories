91d55ad845f1b2fae2201d42ab2cdd61d5008f04
Roja-ucla <roja@ucla.edu>
2012-12-14 20:42:11 -0800

    recovery file

--- Code/createGraphs.sh
+++ Code/createGraphs.sh
@@ -5,7 +5,8 @@ y=30
 
 while [ $x -le $y ]
 do
-  python extract_bipartite_pro.py 12/$x/2008 30
+#  python extract_bipartite_pro.py 12/$x/2008 30
+  python bipartiteMongo.py 09/$x/2006 30 # first vote is on 2006-08-14
   x=$(( $x + 14 ))
 done
 
